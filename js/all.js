bc78dd5279bfe4d1b5f03b61916663dd4c813e01
Ching-wen Hu <ching-wen.hu@developersinstitute.co.nz>
2022-04-18 21:37:59 +1200

    begin all.js - DOM selectors

---
+++ js/all.js
@@ -0,0 +1,9 @@
+// DOM選取元素、宣告
+const districtSelect = document.querySelector(".district-select") //行政區下拉選單
+
+const popularDistrictList = document.querySelector(".popularDistrict_list") //熱門行政區
+
+const districtTitle = document.querySelector(".district_title") //行政區標題
+
+const touristSpotList = document.querySelector(".touristSpot_list") //景點列表
+
