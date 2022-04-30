ea1aac3cf718a342e125871e93170bc4777ee351
Ching-wen Hu <ching-wen.hu@developersinstitute.co.nz>
2022-04-30 17:00:53 +1200

    ediit selectOptionRender and start pagination JS

--- js/all.js
+++ js/all.js
@@ -10,6 +10,9 @@ const touristSpotList = document.querySelector('.touristSpot_list');// 景點列
 
 const toTop = document.querySelector('.toTop');// 點擊回到最上方
 // 撈取資料
+
+const paginator = document.querySelector('#paginator'); // 分頁
+
 let touristSpotData = [];
 async function getData() {
   const url = 'https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json';
@@ -22,6 +25,7 @@ async function getData() {
   // 請求成功就會執行以下function
   init();
   selectOptionList();
+  pagination(touristSpotData.length);
   return touristSpotData;
 }
 getData();
@@ -98,9 +102,18 @@ districtSelect.addEventListener('change', selectOptionRender);
 // 下面這個函式，用來渲染，點選行政區後，頁面需要呈現景點位在該行政區的頁面
 // 當使用者在select下拉選單中，點擊其中一個區 (e.target.value) , 如果跟資料陣列裡的行政區一樣，則會執行頁面渲染
 function selectOptionRender(e) {
+// eslint-disable-next-line max-len
+// 加入下面的程式碼來確保有撈到值。因為第152行，Ｂutton標籤裡面沒 value 屬性（不像select裡的Option裡面已有value屬性），所以要確保都有取到值，我們寫一個名為zone的變數，裡面是空的。使用條件判斷式，如果不等於空值，e.target.value就會被賦予在zone變數裡。如果是空值，那e.target.innerHTML就會被賦予在zone變數裡
+  let zone = '';
+  if (e.target.value !== '') {
+    zone = e.target.value;
+  } else {
+    zone = e.target.innerHTML;
+  }
+  //
   let strSelected = '';
   touristSpotData.forEach((item) => {
-    if (e.target.value === item.Zone) {
+    if (zone === item.Zone) {
       const content = `<li class="mb-5 col-md-6">
             <div class="spot_item h-100">
             <div class="item-header">
@@ -145,7 +158,10 @@ popularDistrictList.addEventListener('click', (e) => {
   // e.preventDefault();
 
   if (e.target.nodeName === 'BUTTON') {
-    console.log('why');
+    // console.log(e.target.value);
+    // console.log(e.target.nodeName)
+    // console.log(e.target.innerHTML)
+    // console.log('why');
     selectOptionRender(e);
   }
 });
@@ -157,4 +173,29 @@ toTop.addEventListener('click', (e) => {
     top: 0,
     behavior: 'smooth',
   });
+});
+
+// Pagination
+const dataPerPage = 8;
+function getDataByPage(page) {
+  const startIndex = (page - 1) * dataPerPage;
+  return touristSpotData.slice(startIndex, startIndex + dataPerPage);
+}
+
+// 計算總頁數並演算 li.page-item/ 算出我們該生產幾個頁，li.page-item
+function pagination(dataTotal) {
+  // 頁面總數量公式：總資料數 / 每一顯示資料 (dataPerPage)
+  const totalPage = Math.ceil(dataTotal / dataPerPage); // 總頁數
+  let rawHTML = '';
+  // eslint-disable-next-line no-plusplus
+  for (let page = 1; page <= totalPage; page++) {
+    rawHTML += `<li class="page-item"><a class="page-link" href="#" data-page="${page}">${page}</a></li>`;
+    paginator.innerHTML = rawHTML;
+  }
+}
+// 加入一個 pagination.addEventListener 事件監聽器。 如果點擊到 a 標籤，就需要呼叫 getDataByPage 根據指定的頁數重新渲染頁面。
+paginator.addEventListener('click', (e) => {
+  if (e.target.nodeName !== 'A') return;
+  const page = Number(e.target.dataset.page);
+  init(getDataByPage(page));
 });
